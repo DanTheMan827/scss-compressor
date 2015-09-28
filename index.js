6ca01291433604b7f5cf0a471624e1b5eace2a09
Daniel <daniel.radtke@gmail.com>
2015-09-28 11:23:11 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var userArgs     =	process.argv.slice(2),
+	cwd          =	process.cwd(),
+	outputPath   =	(function(file){
+						var split = file.split('.')
+						split.pop()
+						return split.join('.')
+					})(userArgs[0]),
+	fs           =	require('fs'),
+	sass         =	require('node-sass'),
+	autoprefixer =	require('autoprefixer')
+	postcss      =	require('postcss'),
+	cleancss     =	require('clean-css');
+	
+	console.log(userArgs[0]);
+	sass.render({
+		file: userArgs[0],
+		config_file: cwd + '\config.rb'
+	}, function(err, result){
+		postcss([ autoprefixer({ browsers: ['> 0%']}) ]).process(result.css).then(function (result) {
+			fs.writeFile(outputPath + '.css', result.css)
+			new cleancss({
+				keepSpecialComments: 0
+			}).minify(result.css, function (errors, minified) {
+				fs.writeFile(outputPath + '.min.css', minified.styles)
+				
+			});
+		});
+	});
---
+++ process_scss.cmd
@@ -0,0 +1,2 @@
+@echo off
+"%~dp0node_modules\.bin\node" "%~dp0\index.js" %1
